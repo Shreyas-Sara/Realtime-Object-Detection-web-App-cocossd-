ad75629a50caf22889fe413dc93073bf208724d2
Shreyas Desai <81674158+Shreyas-Sara@users.noreply.github.com>
2022-02-18 15:26:26 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,52 @@
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage('bedroom.jpg');
+}
+    
+function setup() {
+    canvas = createCanvas(360, 360);
+    canvas.center();
+    detector = ml5.objectDetector('cocossd', modalloaded);
+    document.getElementById("status").innerHTML = "Object Detecting";
+}
+
+function modalloaded() {
+    console.log("Modal Loaded Successfull");
+    status = true;
+    detector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw(){
+    image(img, 0, 0, 360, 360);
+
+    if(status != ""){
+       
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Object Detected";
+           
+            percentage = floor(objects[i].confidence * 100);
+            fill("red");
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("green");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function back(){
+    window.location = "index.html";
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+function f1(){
+    window.location = "bedroom.html";
+}
+
+function f2(){
+    window.location = "tv.html";
+}
+
+function f3(){
+    window.location = "desk.html";
+}
+
+function f4(){
+    window.location = "bottles.html";
+}
+
+function f5(){
+    window.location = "fruits.html";
+}
